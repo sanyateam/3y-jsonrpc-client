66f143c2e7471deefbb1eef7882a0d3192125bb4
chaz6chez <250220719@qq.com>
2020-09-29 15:26:49 +0800

    update
    fixed
    RpcClient::asyncSend
    RpcClient::asyncRecv
    RpcClient::call

--- test/test.php
+++ test/test.php
@@ -6,9 +6,73 @@ require_once ROOT_PATH . '/vendor/autoload.php';
 
 Use JsonRpc\RpcClient;
 
+echo "---- call ----\n";
+$v = RpcClient::instance(['tcp://127.0.0.1:5252'])->call(
+    'Test.Test.demo',
+    ['call' => 'Test.Test.demo'],
+    RpcClient::uuid()
+);
+
+var_dump($v);
+echo "---- call ----\n";
+
+echo "---- call_notice ----\n";
 $v = RpcClient::instance(['tcp://127.0.0.1:5252'])->call('Test.Test.demo',[
-    'a' => '1',
-    'b' => '2'
+    'call' => 'Test.Test.demo',
 ]);
 
 var_dump($v);
+echo "---- call_notice ----\n";
+
+
+//
+//echo "---- async ----\n";
+//try {
+//    $rpc = RpcClient::instance(['tcp://127.0.0.1:5252']);
+//    [$key, $res] = $rpc->asyncSend(
+//        'Test.Test.demo',
+//        ['async' => 'send'],
+//        RpcClient::uuid()
+//    );
+//
+//
+//
+//    if($res){
+//        [$tag, $data] = $rpc->asyncRecv($key);
+//        var_dump($tag);
+//        var_dump($data);
+//    }
+//
+//
+//}catch(\JsonRpc\Exception\MethodAlreadyException $methodAlreadyException){
+//    var_dump($methodAlreadyException);
+//
+//}catch(\JsonRpc\Exception\MethodNotReadyException $methodNotReadyException){
+//    var_dump($methodNotReadyException);
+//}
+//echo "---- async ----\n";
+//
+//echo "---- async_notice ----\n";
+//try {
+//    [$key, $res] = $rpc->asyncNoticeSend(
+//        'Test.Test.demo',
+//        ['async' => 'notice_send'],
+//        false
+//    );
+//    if($res){
+//        [$tag, $data] = $rpc->asyncRecv($key);
+//        var_dump($tag);
+//        var_dump($data);
+//    }
+//
+//
+//}catch(\JsonRpc\Exception\MethodAlreadyException $methodAlreadyException){
+//    var_dump($methodAlreadyException);
+//}catch(\JsonRpc\Exception\MethodNotReadyException $methodNotReadyException){
+//    var_dump($methodNotReadyException);
+//
+//}
+//echo "---- async_notice ----\n";
+
+
+
