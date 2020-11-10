91a0816d399d3af24f7ae91e742dab44eb9dfe70
chaz6chez <250220719@qq.com>
2020-11-10 15:44:37 +0800

    修复RpcClient异步发送服务执行失败错误

--- test/test.php
+++ test/test.php
@@ -6,6 +6,11 @@ require_once ROOT_PATH . '/vendor/autoload.php';
 
 Use JsonRpc\RpcClient;
 
+function getCurrentTime() {
+    list ($msec, $sec) = explode(" ", microtime());
+    return (float)$msec + (float)$sec;
+}
+
 //echo "---- send ----\n";
 //$client = RpcClient::instance(['tcp://localhost:5252']);
 //var_dump(RpcClient::getInstance());
@@ -23,87 +28,72 @@ Use JsonRpc\RpcClient;
 //var_dump($v);
 //echo "---- send ----\n";
 
-echo "---- call ----\n";
-$v = RpcClient::instance(['tcp://localhost:5252'])->call(
-    'Reject.Test.demo',
-    ['call' => 'Reject.Test.demo'],
-    RpcClient::uuid()
-);
-
-var_dump($v);
-
-$v = RpcClient::instance(['tcp://localhost:5252'])->call(
-    'Test.Test.demo',
-    ['call' => 'Test.Test.demo'],
-    RpcClient::uuid()
-);
-
-var_dump($v);
-
-$v = RpcClient::instance(['tcp://localhost:5252'])->call(
-    'MethodNotFound.Test.demo',
-    ['call' => 'MethodNotFound.Test.demo'],
-    RpcClient::uuid()
-);
-
-var_dump($v);
-echo "---- call ----\n";
-
-//echo "---- call_notice ----\n";
-//$v = RpcClient::instance(['tcp://localhost:5252'])->call('Test.Test.demo',[
-//    'call' => 'Test.Test.demo',
-//]);
+//echo "---- call ----\n";
+//$v = RpcClient::instance(['tcp://localhost:5252'])->call(
+//    'Reject.Test.demo',
+//    ['call' => 'Reject.Test.demo'],
+//    RpcClient::uuid()
+//);
 //
 //var_dump($v);
+//
+//$v = RpcClient::instance(['tcp://localhost:5252'])->call(
+//    'Test.Test.demo',
+//    ['call' => 'Test.Test.demo'],
+//    RpcClient::uuid()
+//);
+//
+//var_dump($v);
+//
+//$v = RpcClient::instance(['tcp://localhost:5252'])->call(
+//    'MethodNotFound.Test.demo',
+//    ['call' => 'MethodNotFound.Test.demo'],
+//    RpcClient::uuid()
+//);
+//
+//var_dump($v);
+//echo "---- call ----\n";
+
+//echo "---- call_notice ----\n";
+//$v = RpcClient::instance(['tcp://localhost:5252'])->call('Server.UserServer.checker',[
+//    'call' => 'Server.UserServer.checker',
+//]);
 //echo "---- call_notice ----\n";
 
+echo "---- async ----\n";
+$start = getCurrentTime();
+try {
+    $rpc = RpcClient::instance(['tcp://localhost:5252']);
+    [$key1, $res] = $rpc->asyncSend(
+        'Server.UserServer.checker',
+        ['async' => 'send1'],
+        RpcClient::uuid()
+    );
 
+    [$key2, $res] = $rpc->asyncSend(
+        'Server.UserServer.checker1',
+        ['async' => 'send2'],
+        RpcClient::uuid()
+    );
 
-//echo "---- async ----\n";
-//try {
-//    $rpc = RpcClient::instance(['tcp://localhost:5252']);
-//    [$key1, $res] = $rpc->asyncSend(
-//        'Test.Test.demo',
-//        ['async' => 'send1'],
-//        RpcClient::uuid()
-//    );
-//
-//    var_dump('abc1');
-//
-//    [$key2, $res] = $rpc->asyncSend(
-//        'Test.Test.demo',
-//        ['async' => 'send2'],
-//        RpcClient::uuid()
-//    );
-//
-//    var_dump('abc2');
-//
-//    [$key3, $res] = $rpc->asyncSend(
-//        'Test.Test.demo',
-//        ['async' => 'send3'],
-//        RpcClient::uuid()
-//    );
-//
-//    var_dump('abc3');
-//
-//    [$tag, $data] = $rpc->asyncRecv($key1);
-//    var_dump($tag);
-//    var_dump($data);
-//    [$tag, $data] = $rpc->asyncRecv($key2);
-//    var_dump($tag);
-//    var_dump($data);
-//    [$tag, $data] = $rpc->asyncRecv($key3);
-//    var_dump($tag);
-//    var_dump($data);
-//
-//
-//}catch(\JsonRpc\Exception\MethodAlreadyException $methodAlreadyException){
-//    var_dump($methodAlreadyException);
-//
-//}catch(\JsonRpc\Exception\MethodNotReadyException $methodNotReadyException){
-//    var_dump($methodNotReadyException);
-//}
-//echo "---- async ----\n";
+    [$tag, $data] = $rpc->asyncRecv($key1);
+    var_dump($tag);
+    var_dump($data);
+
+    [$tag, $data] = $rpc->asyncRecv($key2);
+    var_dump($tag);
+    var_dump($data);
+
+    $rpc->close();
+
+}catch(\JsonRpc\Exception\MethodAlreadyException $methodAlreadyException){
+    var_dump($methodAlreadyException);
+
+}catch(\JsonRpc\Exception\MethodNotReadyException $methodNotReadyException){
+    var_dump($methodNotReadyException);
+}
+echo getCurrentTime() - $start . PHP_EOL;
+echo "---- async ----\n";
 
 //echo "---- async_notice ----\n";
 //try {
@@ -117,6 +107,7 @@ echo "---- call ----\n";
 //        var_dump($tag);
 //        var_dump($data);
 //    }
+//    $rpc->close();
 //
 //
 //}catch(\JsonRpc\Exception\MethodAlreadyException $methodAlreadyException){
